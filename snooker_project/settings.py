fb5b3b584eec32e7fba77c1b7616ee182a257553
zyniu81 <zyniu81@wp.pl>
2026-02-12 10:26:25 +0100

    Zmiana na Port 465 SSL + Timeout 30s

--- snooker_project/settings.py
+++ snooker_project/settings.py
@@ -185,9 +185,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Configuration email (Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
